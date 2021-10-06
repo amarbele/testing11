37614bb29e7a017cfe0fb0e7b3ed5b284df8e206
Hp <Hp@DESKTOP-TPCCBRL>
2021-10-06 12:26:10 +0530

    By ABD1

--- Git_Project_061021/src/cjc/com/DemoGit.java
+++ Git_Project_061021/src/cjc/com/DemoGit.java
@@ -2,6 +2,11 @@ package cjc.com;
 
 public class DemoGit {
 
+	public void m1()
+	{
+		System.out.println("Hello this is m1");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("hello Git");
