41728af7af006e01d80ffb399400d8c9d3be76d8
Hp <Hp@DESKTOP-TPCCBRL>
2021-10-07 15:30:44 +0530

    by adb2

--- Git_project_2_061021/src/com/cjc/demo1/Demo2.java
+++ Git_project_2_061021/src/com/cjc/demo1/Demo2.java
@@ -2,9 +2,12 @@ package com.cjc.demo1;
 
 public class Demo2 
 {
+	public void m1()
+	{
+		System.out.println("this is m1 Method");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("hello cjc");
 	}
- 
 }
